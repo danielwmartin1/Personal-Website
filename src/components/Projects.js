081aecf04d5af01f3ee5603adecb53967dae4277
danielwmartin1 <danielwmartin1@gmail.com>
2025-01-23 05:43:48 -0500

    Update E-Commerce project link text for clarity

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,7 +20,7 @@ const Projects = () => {
         { href: "https://dwm-tictactoe.vercel.app/", text: "Tic-Tac-Toe" },
         { href: "https://dwm-simonsays.vercel.app/", text: "Simon Says" },
         { href: "https://acls-algorithm.vercel.app/", text: "ACLS Algorithm" },
-        { href: "https://dwm-ecommerce.vercel.app/", text: "E-Commerce Frontend" },
+        { href: "https://dwm-ecommerce.vercel.app/", text: "E-Commerce (Frontend)" },
         { href: "https://dwm-weatherapp.vercel.app/", text: "Weather App" },
         { href: "https://dwm-todolist.vercel.app/", text: "React.JS To-Do List" },
         { href: "https://dwm-todolist-api.vercel.app/tasks/", text: "React.JS To-Do List API" },
